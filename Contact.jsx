02baadf89302de219522bcbad0e01e1e45eb66ae
Krystle Murphy <120310009+KrystleMurphy@users.noreply.github.com>
2024-04-09 21:20:03 +0100

    adding color changes to a tags

--- Contact.jsx
+++ Contact.jsx
@@ -12,78 +12,51 @@ import ByMeACoffee from "./ByMeACoffee";
 export default function Contact() {
   return (
     <>
-      <div className="container align-items-center justify-content-center text-center">
+      <div className="container text-center">
         <div className="row">
           <h1 className="pt-3">Get in Touch</h1>
         </div>
-
-
-        <div className="row d-flex justify-content-center">
+        <div className="row justify-content-center">
           <div className="col-2">
-              <div className="card border-0 card-body">
-                <a
-                  href="https://www.linkedin.com/in/krystle-murphy/"
-                  target="_blank"
-                  className="rounded text-black p-3"
-                >
-                  <h5 className="card-title">
-                    <FontAwesomeIcon icon={faLinkedin} size="3x" />
-                  </h5>
-                  Visit Profile
-                </a>
+            <div className="card border-0 card-body">
+              <a
+                href="https://www.linkedin.com/in/krystle-murphy/"
+                target="_blank"
+                className="rounded text-black p-3 d-block"
+              >
+                <h5 className="card-title">
+                  <FontAwesomeIcon icon={faLinkedin} size="3x" />
+                </h5>
+                Visit Profile
+              </a>
             </div>
           </div>
-
-        {/* <div className="row d-flex justify-content-center">
-          <div className="col-2 mb-3 mb-sm-0">
-            <div className="card border-0">
-              <div className="card-body">
-                <a
-                  href="https://www.linkedin.com/in/krystle-murphy/"
-                  target="_blank"
-                  className="btn rounded"
-                  style={{
-                    backgroundColor: "var(--custom-blue)",
-                  }}
-                >
-                  <h5 className="card-title">
-                    <FontAwesomeIcon icon={faLinkedin} size="3x" />
-                  </h5>
-                  Visit Profile
-                </a>
-              </div>
-            </div>
-          </div> */}
-
-
           <div className="col-2">
-            <div className="card border-0">
-              <div className="card-body">
-                <a
-                  href="https://github.com/KrystleMurphy"
-                  target="_blank"
-                  className="btn rounded"
-                  style={{ backgroundColor: "var(--custom-blue)" }}>
-                  <h5 className="card-title">
-                    <FontAwesomeIcon icon={faGithub} size="3x" />
-                  </h5>
-                  Take a look
-                </a>
-              </div>
+            <div className="card border-0 card-body">
+              <a
+                href="https://github.com/KrystleMurphy"
+                target="_blank"
+                className="rounded text-black p-3 d-block"
+              >
+                <h5 className="card-title">
+                  <FontAwesomeIcon icon={faGithub} size="3x" />
+                </h5>
+                Take a look
+              </a>
             </div>
           </div>
         </div>
-      </div>
-
-      <div className="row justify-content-center align-items-center">
-<ByMeACoffee />
-</div>
-      <div className="p-3 d-flex justify-content-center align-items-center">
-        <div className="row col-6 rounded p-3" style={{border: "1px solid var(--custom-blue)"}}>
-          <ContactForm />
+        <div className="row justify-content-center align-items-center">
+          <ByMeACoffee />
+        </div>
+        <div className="p-3">
+          <div className="row justify-content-center">
+            <div className="col-6 rounded p-3" style={{border: "1px solid var(--custom-blue)"}}>
+              <ContactForm />
+            </div>
+          </div>
         </div>
       </div>
     </>
   );
 }
-
--- ContactForm.jsx
+++ ContactForm.jsx
@@ -7,46 +7,51 @@ function ContactForm() {
       return <p>Thanks for reaching out! I will be in contact soon</p>;
   }
   return (
-    
-      <form onSubmit={handleSubmit}>
-        <div className="row p-3">
-      <label htmlFor="email" className="">Email Address:
-      </label>
-
-      <input
-        id="email"
-        type="email" 
-        name="email"
-        placeholder='MyEmail@example.com'
-        className='rounded'
-        style={{border: "1px solid var(--custom-blue)"}}
-      />
-      <ValidationError 
-        prefix="Email" 
-        field="email"
-        errors={state.errors}
-      /></div>
+    <form onSubmit={handleSubmit}>
+      <div className="row p-3">
+        <label htmlFor="email" className="col-3">
+          Email Address:
+        </label>
+        <input
+          id="email"
+          type="email" 
+          name="email"
+          placeholder='MyEmail@example.com'
+          className='rounded col-9' // Adjusted the input width
+          style={{border: "1px solid var(--custom-blue)"}}
+        />
+        <ValidationError 
+          prefix="Email" 
+          field="email"
+          errors={state.errors}
+        />
+      </div>
 
       <div className="row p-3">
-        Message:
-      <textarea
-        id="message"
-        name="message"
-        placeholder='Hi there...'
-        className='rounded'
-        style={{border: "1px solid var(--custom-blue)"}}
-      />
-      <ValidationError 
-        prefix="Message" 
-        field="message"
-        errors={state.errors}
-      /></div>
-      <div className="row  p-3 justify-content-center" >
-      <button className="btn col-3 rounded" style={{ backgroundColor: "var(--custom-blue)" }} type="submit" disabled={state.submitting}>
-        Submit
-      </button></div>
+        <label htmlFor="message" className="col-3">
+          Message:
+        </label>
+        <textarea
+          id="message"
+          name="message"
+          placeholder='Hi there...'
+          className='rounded col-9' // Adjusted the textarea width
+          style={{border: "1px solid var(--custom-blue)"}}
+        />
+        <ValidationError 
+          prefix="Message" 
+          field="message"
+          errors={state.errors}
+        />
+      </div>
+      
+      <div className="row p-3 justify-content-center">
+        <a className="rounded text-black" type="submit" disabled={state.submitting}>
+          Submit
+        </a>
+      </div>
     </form>
   );
 }
 
-export default ContactForm
+export default ContactForm;
